daa2ee463973f9aa0d19d0aa64b05b64f33664c9
zFerchou <luisfernandoma94@gmail.com>
2025-06-05 19:40:24 -0600

    creacion de layout

---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Content from './Content';
+import Menu from './Menu';
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Content />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
