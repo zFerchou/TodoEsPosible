9357ef7271bf16f3d00ddf6adacb9115046fbc1b
zFerchou <luisfernandoma94@gmail.com>
2025-06-05 19:39:33 -0600

    creacion de menu

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <nav style={{ backgroundColor: '#f8f9fa', padding: '1rem' }}>
+      <a href="/">Inicio</a> | <a href="/acerca">Acerca de</a>
+    </nav>
+  );
+};
+
+export default Menu;
