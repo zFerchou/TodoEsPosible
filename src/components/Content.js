1f64147ed20767e4aefb49b9e3324b0a2099a641
zFerchou <luisfernandoma94@gmail.com>
2025-06-05 19:21:53 -0600

    creacion de content

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <main style={{ padding: '1rem' }}>
+      <p>Bienvenido a la aplicación TodoEsPosible. Aquí irá el contenido principal.</p>
+    </main>
+  );
+};
+
+export default Content;
