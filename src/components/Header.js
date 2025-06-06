f5d12594d1612715de323ba87aaf3ff87bcdf318
zFerchou <luisfernandoma94@gmail.com>
2025-06-05 18:56:14 -0600

    Agregar componente Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: '#007bff', color: 'white', padding: '1rem' }}>
+      <h1>TodoEsPosible</h1>
+    </header>
+  );
+};
+
+export default Header;
